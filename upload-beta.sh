a52697a9d9c59c689eb2271373de2dd9ba022efe
Froggo <92762044+Froggo8311@users.noreply.github.com>
2022-05-26 18:42:20 +0000

    Final cleanup of name fix

--- upload-beta.sh
+++ upload-beta.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python setup.py sdist bdist_wheel
 python -m twine upload --repository testpypi dist/* --verbose
-rm -rf dist/ build/ pheonix.egg-info/
+rm -rf dist/ build/ phoenix_ws.egg-info/ phoenix/__pycache__ ppm/__pycache__
