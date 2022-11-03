6ff8019fddceef5f2d1d314d5ace14db6e8590f8
Froggo8311 <Froggo8311@localhost>
2022-11-03 15:31:16 -0500

    Update upload scripts for easier GitHub releases

---
+++ production-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python setup.py sdist bdist_wheel
+python -m twine upload dist/* --verbose
+mv dist/*.whl .
+echo "Done. You can now upload the wheel to GitHub."
+read -p "Press Enter to delete the wheel." </dev/tty
+rm -rf dist/ build/ *.egg-info/ pheonix/__pycache__ ppm/__pycache__ __pycache__ *.whl
